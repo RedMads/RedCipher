09552e5ff671ea312a4284b2df7b68b12a61dfdf
RedMads <67266879+RedMads@users.noreply.github.com>
2021-06-20 17:14:05 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ fi
 
 # install some good things !
 apt update -y && apt upgrade -y
-apt install git python python3 python3-pip -y
+apt install git python python3 python3-pip rust -y
 
 pip3 install pycryptodome
 pip3 install cryptography
