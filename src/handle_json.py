0eecced5f5170665244cf74936730b6b50545de3
RedMads <67266879+RedMads@users.noreply.github.com>
2021-08-21 03:37:36 -0400

    add new default settings

--- src/handle_json.py
+++ src/handle_json.py
@@ -1,5 +1,5 @@
 import json
-
+import os
 
 class Handle_json:
 
@@ -7,14 +7,16 @@ class Handle_json:
 
         self.settings = {}
 
+        self.settings_path = os.path.dirname(__file__).replace("/src","") + "/settings.json"
+
     
     # This function load the settings file !
-    def load_json(self, filename):
+    def load_json(self):
 
         try:
 
             # Try to read the < settings.json > file !
-            with open(filename, "r") as json_file:
+            with open(self.settings_path, "r") as json_file:
 
                 self.settings = json.loads(json_file.read())
 
@@ -26,17 +28,11 @@ class Handle_json:
             self.settings = {
                 "settings": {
 
-                    "salt": "Abo_Na9r!L*C3%T1GgZ#KqSDYMzi64hS#ilDMBaN*4C0K98MdV#GXl^HIm!",
                     "separator": "#####",
                     "extention": ".redc",
-                    "key_size": 4096
+                    "key_size": 2048
                 }
             }
-
-
-    def get_salt(self):
-
-        return self.settings["settings"]["salt"]
 
     
     def get_separator(self):
@@ -58,8 +54,6 @@ class Handle_json:
 if __name__ == '__main__':
 
     h_obj = Handle_json()
-
-    h_obj.load_json("settings.json")
 
     print(f"Salt: {h_obj.get_salt()}")
     print(f"Separator: {h_obj.get_separator()}")
