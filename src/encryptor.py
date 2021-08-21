a2b65183917a66edc6e12bc3b958529e75921701
RedMads <67266879+RedMads@users.noreply.github.com>
2021-08-21 03:35:50 -0400

    Remove cryptography moduel
    Remove cryptography moduel and replace it with pycryptodome

--- src/encryptor.py
+++ src/encryptor.py
@@ -1,17 +1,16 @@
 
 # Some Great librarys!!!!!!
+from Crypto.Cipher import AES
 from Crypto.PublicKey import RSA
 from Crypto import Random
 from Crypto.Cipher import PKCS1_OAEP 
-from cryptography.fernet import Fernet
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-from base64 import b64decode, b64encode, urlsafe_b64encode
+from base64 import b64decode, b64encode, urlsafe_b64encode, urlsafe_b64decode
 from getpass import getuser
 from os import mkdir, listdir, chdir, getcwd, urandom, path, remove
 from pathlib import Path
 from src.handle_json import Handle_json
+from src.aes_encryptor import AES_encryptor
+import hashlib
 
 
 # The Main class!
@@ -20,9 +19,10 @@ class Encryptor:
     def __init__(self):
 
         self.h_obj = Handle_json()
-        self.h_obj.load_json("settings.json")
+        self.a_obj = AES_encryptor()
 
-        self.salt = self.h_obj.get_salt()
+        self.h_obj.load_json()
+
         self.sep = self.h_obj.get_separator()
         self.keys_size = self.h_obj.get_keysize()
 
@@ -68,58 +68,21 @@ class Encryptor:
 
 
 
-    # This Function take a string and convert it to  Fernet Encoded Key !
-    # Args < password: String >                                          
-
-    def password_to_fernet_key(self, password):
-
-
-        kdf = PBKDF2HMAC(
-            algorithm=hashes.SHA256(),
-            length=32,
-            salt= self.salt.encode(),
-            iterations=100000,
-            backend= default_backend()
-        )
-
-
-        return urlsafe_b64encode(kdf.derive(password.encode("utf-8")))
-
-
-    # This Function Encrypt a string with Fernet Key !
-    # Args < message: String > && < key: Fernet Encoded Key >
-
-    def fernet_encrypt(self,message, key ):
-
-        cipher = Fernet(key); return cipher.encrypt(message)
-
-
-    
-    # This Function Decrypt a encrypted string with Fernet Key !
-    # Args < enc_message: String > && < key: Fernet Encoded Key >
-
-    def fernet_decrypt(self, enc_message, key):
-
-        cipher = Fernet(key); return cipher.decrypt(enc_message)
-
-
-
     # This Function Generate RSA keys and write them in files !
     # Args < KeySize: int / default: 4096 >
     # Notice: the keys size was taken from constructor function up! 
 
     def generate_keys(self):
-                    
-        private = RSA.generate(self.keys_size)
 
+        private = RSA.generate(self.keys_size)
 
         with open(self.private_key_file, "wb") as private_file:
                     
             private_file.write(private.export_key())
                     
         with open(self.public_key_file, "wb") as public_file:
-                    
-            public_file.write(private.publickey().export_key())        
+
+            public_file.write(private.publickey().export_key())
 
 
 
@@ -131,9 +94,9 @@ class Encryptor:
 
         key = RSA.import_key(open(self.public_key_file).read()); rsa_cipher = PKCS1_OAEP.new(key)
 
-        aes_key = Fernet.generate_key(); aes_cipher = Fernet(aes_key)
+        aes_key = self.a_obj.generate_key("None"); encrypted_data = self.a_obj.encrypt(message, aes_key)
 
-        encrypted_data = aes_cipher.encrypt(message); encrypted_aes_key = rsa_cipher.encrypt(aes_key)
+        encrypted_aes_key = rsa_cipher.encrypt(aes_key)
 
         full_encrypted = b64encode(encrypted_aes_key).decode() + self.sep + b64encode(encrypted_data).decode()
 
@@ -152,9 +115,9 @@ class Encryptor:
 
         enc_aes_key = enc_message.decode().split(self.sep)[0]; aes_key = rsa_cipher.decrypt(b64decode(enc_aes_key))
 
-        dec_data = enc_message.decode().split(self.sep)[1]; aes_cipher = Fernet(aes_key)
+        dec_data = enc_message.decode().split(self.sep)[1]
 
-        return aes_cipher.decrypt(b64decode(dec_data))
+        return self.a_obj.decrypt(b64decode(dec_data.encode()), aes_key)
 
 
 
@@ -165,13 +128,14 @@ class Encryptor:
 
         key = RSA.import_key(open(pub_key_path).read()); rsa_cipher = PKCS1_OAEP.new(key)
 
-        aes_key = Fernet.generate_key(); aes_cipher = Fernet(aes_key)
+        aes_key = self.a_obj.generate_key("None"); encrypted_data = self.a_obj.encrypt(message, aes_key)
 
-        encrypted_data = aes_cipher.encrypt(message); encrypted_aes_key = rsa_cipher.encrypt(aes_key)
+        encrypted_aes_key = rsa_cipher.encrypt(aes_key)
 
         full_encrypted = b64encode(encrypted_aes_key).decode() + self.sep + b64encode(encrypted_data).decode()
 
         return full_encrypted
+
 
     # This Function Decrypt a Encrypted String with loaded RSA private key !
     # Args < enc_message: Bytes > & < priv_key_path: String > 
@@ -182,12 +146,10 @@ class Encryptor:
 
         enc_aes_key = enc_message.decode().split(self.sep)[0]; aes_key = rsa_cipher.decrypt(b64decode(enc_aes_key))
 
-        dec_data = enc_message.decode().split(self.sep)[1]; aes_cipher = Fernet(aes_key)
+        dec_data = enc_message.decode().split(self.sep)[1]
 
-        return aes_cipher.decrypt(b64decode(dec_data))
+        return self.a_obj.decrypt(b64decode(dec_data.encode()), aes_key)
             
-
-
 
 
 # Just a Test Code ! :$ 
