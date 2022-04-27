0a4f395e2dafaac29bd359a74d83943cea70dcfd
Zaky <74756489+Zaky202@users.noreply.github.com>
2022-04-27 15:33:07 -0700

    Create install_linux.sh

---
+++ installers/install_linux.sh
@@ -0,0 +1,18 @@
+
+#check if this script run as root or not
+if [ "$EUID" -ne 0 ]; then
+
+    echo "[!] Run this script as root!"
+    exit
+
+fi
+
+# install some good things !
+apt update -y && apt upgrade -y
+apt install git python python3 python3-pip rust -y
+
+pip3 install pycryptodome
+pip3 install cryptography
+pip3 install argparse
+pip3 install coloredlogs==15.0.1
+
