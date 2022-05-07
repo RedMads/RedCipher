3c0369115b131b90fbc9e33443c5c53d21938e7f
RedMads <o7da.ksa@gmail.com>
2022-05-07 11:54:29 +0300

    remove installers
    remove installers (replaced with requirements.txt file)

--- installers/install_windows.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo ===========================
-echo = installing py librarys  =
-echo ===========================
-pip3 install pycryptodome
-pip3 install cryptography
-pip3 install argparse
-pip3 install coloredlogs==15.0.1
