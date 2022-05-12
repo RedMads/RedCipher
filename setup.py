bbbb35ae1ea7000696f31108575f744a3f428ce9
RedMads <o7da.ksa@gmail.com>
2022-05-12 22:52:19 +0300

    add setup.py
    it's useful file specially for pypi packaging

---
+++ setup.py
@@ -0,0 +1,60 @@
+from asyncore import read
+from numpy import require
+from setuptools import setup, find_packages
+
+__name__ = "redcipher"
+__version__ = "v1.0.5"
+__author__ = "RedMads"
+__author_email__ = "redmads@protonmail.com"
+__date__ = "2022-5-12"
+
+
+with open("README.md", "r", encoding="utf-8") as readme:
+    long_description = readme.read()
+
+with open("requirements.txt", "r") as reqFile:
+
+    requirements = []
+
+    for line in reqFile.readlines():
+        requirements.append(line[:-1])
+
+readme.close()
+reqFile.close()
+
+
+setup(
+    name=__name__,
+    version=__version__,
+    description='Professional Encryption / Decryption program !',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/RedMads/RedCipher/',
+    author=__author__,
+    author_email=__author_email__,
+
+    project_urls={
+      'Source': 'https://github.com/RedMads/RedCipher',
+      'Report Bugs': 'https://github.com/RedMads/RedCipher/issues',
+      'Download': 'https://pypi.org/project/redcipher/#files',
+      'Documentation': 'https://github.com/RedMads/RedCipher/blob/master/README.md'
+    },
+
+    license='MIT',
+
+    keywords=[
+        "python", "encrypt", "decrypt",
+        "cipher", "cryptography", "crypto",
+        "AES", "RSA", "decryption", "encryption"
+       ],
+
+    packages=find_packages(),
+
+    install_requires=requirements,
+
+    entry_points={
+        'console_scripts': [
+                "redc=red_cipher.__main__:main",
+        ]
+	}
+)
